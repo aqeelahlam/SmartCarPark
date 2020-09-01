7588a4ecb10cd5c5e6fb5be7b628eaea026d3168
kbee0002 <kbee0002@student.monash.edu>
2020-09-01 16:24:56 +0800

    feature/layout_of_carpark

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Smart Car Park',
+      theme: new ThemeData(scaffoldBackgroundColor: Colors.black54),
+//      theme: ThemeData(
+//        // This is the theme of your application.
+//        //
+//        // Try running your application with "flutter run". You'll see the
+//        // application has a blue toolbar. Then, without quitting the app, try
+//        // changing the primarySwatch below to Colors.green and then invoke
+//        // "hot reload" (press "r" in the console where you ran "flutter run",
+//        // or simply save your changes to "hot reload" in a Flutter IDE).
+//        // Notice that the counter didn't reset back to zero; the application
+//        // is not restarted.
+//        primarySwatch: Colors.blue,
+//        // This makes the visual density adapt to the platform that you run
+//        // the app on. For desktop platforms, the controls will be smaller and
+//        // closer together (more dense) than on mobile platforms.
+//        visualDensity: VisualDensity.adaptivePlatformDensity,
+//      ),
+      home: MyHomePage(title: 'Smart Car Park'),
     );
   }
 }
@@ -63,6 +64,9 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+
+
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -71,47 +75,149 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+        appBar: new AppBar(
+        backgroundColor: Colors.black54,
+          title: const Text('Smart Car Park',
+            style: TextStyle(
+                fontSize: 35.0
+            ),),
+    ),
+    body: Column(children: <Widget>[
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          EmptyBox(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ParkingSlot(true, 'A1'),
+          ParkingSlot(false, 'B1'),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ParkingSlot(true, 'A2'),
+          ParkingSlot(true, 'B2'),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ParkingSlot(false, 'A3'),
+          ParkingSlot(false, 'B3'),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ParkingSlot(false, 'A4'),
+          ParkingSlot(true, 'B4'),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ParkingSlot(true, 'A5'),
+          ParkingSlot(true, 'B5'),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          EmptyBox(),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Entrance(),
+        ],
+      )
+
+    ]
+    )
     );
   }
 }
+
+
+class ParkingSlot extends StatelessWidget {
+  bool occupied;
+  String slotNumber;
+  ParkingSlot(bool occupied, slotNumber) {
+    this.occupied = occupied;
+    this.slotNumber = slotNumber;
+  }
+  @override
+  Widget build(BuildContext context) {
+    if (occupied == false){
+      return Container(
+        width: 250,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.green,
+          border: Border.all(),
+        ),
+        child: new Text(slotNumber,
+        style: TextStyle(
+            fontSize: 20.0
+        ),
+      ),
+      );
+    }
+    else {
+      return Container(
+        width: 250,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          border: Border.all(),
+        ),
+        child: new Text(slotNumber,
+          style: TextStyle(
+              fontSize: 20.0
+          ),
+        ),
+      );
+    }
+
+  }
+}
+
+
+class EmptyBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 500,
+      height: 50,
+      alignment: Alignment(0.0, -0.1),
+    );
+  }
+}
+
+class Entrance extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 50,
+      alignment: Alignment(0.0, -0.1),
+      decoration: BoxDecoration(
+        color: Colors.black54,
+        border: Border.all(),
+      ),
+      child: new Text('Entrance',
+        style: TextStyle(
+            fontSize: 20.0,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
