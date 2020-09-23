53f58b15bf76ec99b7f74e6ab39f9fc3d63ffc53
kbee0002 <kbee0002@student.monash.edu>
2020-09-23 13:35:45 +0800

    chore/comments

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,29 @@ import 'package:flutter/widgets.dart';
 import 'package:flushbar/flushbar.dart';
 import 'dart:math';
 
+/*
+  Main script for web dashboard. Dashboard shows the parking slots available on
+  both floors. Green indicates available, and red indicates taken.
+
+  The bottom menu bar allows users to switch between the floors.
+  Clicking on a parking slot will indicate the availability of the slot in a
+  snackbar. Clicking on an available parking slot will also produce arrow directions
+  from the entrance to the parking slot.
+
+  Created August 2020
+  by Bee Khee Siang
+*/
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Smart Car Park',
       theme: new ThemeData(scaffoldBackgroundColor: Colors.black54),
-//      theme: ThemeData(
-//        // This is the theme of your application.
-//        //
-//        // Try running your application with "flutter run". You'll see the
-//        // application has a blue toolbar. Then, without quitting the app, try
-//        // changing the primarySwatch below to Colors.green and then invoke
-//        // "hot reload" (press "r" in the console where you ran "flutter run",
-//        // or simply save your changes to "hot reload" in a Flutter IDE).
-//        // Notice that the counter didn't reset back to zero; the application
-//        // is not restarted.
-//        primarySwatch: Colors.blue,
-//        // This makes the visual density adapt to the platform that you run
-//        // the app on. For desktop platforms, the controls will be smaller and
-//        // closer together (more dense) than on mobile platforms.
-//        visualDensity: VisualDensity.adaptivePlatformDensity,
-//      ),
       home: MyHomePage(title: 'Smart Car Park'),
     );
   }
@@ -38,15 +34,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -66,12 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     layoutSize().init(context);
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
         appBar: new AppBar(
         backgroundColor: Colors.black54,
@@ -80,8 +61,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 fontSize: 35.0
             ),),
     ),
-    body: new _FloorLayout()._getFloorLayout(_selectedIndex),
-    bottomNavigationBar: BottomNavigationBar(
+    body: new _FloorLayout()._getFloorLayout(_selectedIndex), //gets the layout of the correct floor
+    bottomNavigationBar: BottomNavigationBar( //contents of the bottom navigation bar
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
           icon: Icon(Icons.directions_car),
@@ -103,6 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
 class _FloorLayout {
+  // List containing the different layouts
   static List<Card> _layoutOptions =
     <Card>[
       Card(
@@ -244,6 +226,7 @@ class _FloorLayout {
   ];
 
   Card _getFloorLayout(int index) {
+    // function that returns the layout
     return _layoutOptions.elementAt(index);
   }
 }
@@ -298,6 +281,7 @@ class ShapePainter extends CustomPainter {
 
 
 class Road extends StatelessWidget {
+  // StatelessWidget of the road
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -317,9 +301,12 @@ class Road extends StatelessWidget {
 
 
 class ParkingSlot extends StatelessWidget {
+  // StatelessWidget of the Parking slot
   bool occupied;
   String slotNumber;
   String rowNumber;
+
+  // Constructor of a parking slot
   ParkingSlot(bool occupied, slotNumber, rowNumber) {
     this.occupied = occupied;
     this.slotNumber = slotNumber;
@@ -327,8 +314,9 @@ class ParkingSlot extends StatelessWidget {
   }
   @override
   Widget build(BuildContext context) {
+    // if the parking slot is available
     if (occupied == false){
-      return GestureDetector(
+      return GestureDetector( //onclick produces flushbar with details of parking slot
           onTap: () {
             Flushbar(
               title: "Parking Slot: " + slotNumber,
@@ -364,6 +352,7 @@ class ParkingSlot extends StatelessWidget {
       );
     }
     else {
+      // if the parking slot is occupied
       return GestureDetector(
           onTap: () {
             Flushbar(
@@ -405,6 +394,7 @@ class ParkingSlot extends StatelessWidget {
 
 
 class EmptyBox extends StatelessWidget {
+  // StatelessWidget of an empty box. Used to fill up spaces for alignment purposes
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -416,6 +406,7 @@ class EmptyBox extends StatelessWidget {
 }
 
 class Entrance extends StatelessWidget {
+  // StatelessWidget of the entrance
   @override
   Widget build(BuildContext context) {
     return Container(
