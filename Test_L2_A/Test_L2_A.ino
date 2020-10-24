b4ba1f816f2dcac085887a7067637e8c8246fd47
Bee Khee Siang <kbee0002@student.monash.edu>
2020-10-25 02:48:51 +1100

    Unit Testing

--- Test_L2_A/Test_L2_A.ino
+++ Test_L2_A/Test_L2_A.ino
@@ -1,8 +1,7 @@
 #include <MultiShiftRegister.h>
-#include <Servo.h> 
 
 /*
-  Arduino Controlling Slots L1_B1, L1_B2, L1_B3, L1_B4
+  Test script for Arduino Controlling Slots in Level Two(2): L2_A1, L2_A2, L2_A3, L2_A4
 
   Four Ultrasonic Sensors controlling their respective LED and transmits
   the data using Serial Communication to the Raspberry Pi with a baudrate of 9600:
@@ -17,11 +16,9 @@
   - Shift Regist 74HC595
   - RGB light emitting diode (LED)
   - Resistor: 220 Ohm
-  - Servo Motor
 
   created 2020
-  created by Aqeel Ahlam
-  edited by Bee Khee Siang
+  created by Bee Khee Siang
 
 */
 
@@ -30,31 +27,23 @@ int dataPin = 11;   // Data Pin connected to arduino digital pin 11
 int latchPin  = 12; // Latch Pin connected to arduino digital pin 12
 int clockPin = 13;  // Clock Pin connected to arduino digital pin 13
 
-// Initialize a Servo Object for the connected servo 
-Servo servo_exit;        
 
-// Initialize the angle
-int angle = 0;    
-
-// Ultrasonic Sensor L1_B1
+// Ultrasonic Sensor 1
 #define trigPin1 0  // Attach pin 0 Arduino to pin Trig of HC-SR04
 #define echoPin1 1  // Attach pin 1 Arduino to pin Echo of HC-SR04
 
-// Ultrasonic Sensor L1_B2
+// Ultrasonic Sensor 2
 #define trigPin2 2  // Attach pin 2 Arduino to pin Trig of HC-SR04
 #define echoPin2 3  // Attach pin 3 Arduino to pin Echo of HC-SR04
 
-// Ultrasonic Sensor L1_B3
+// Ultrasonic Sensor 3
 #define trigPin3 4  // Attach pin 4 Arduino to pin Trig of HC-SR04
 #define echoPin3 5  // Attach pin 5 Arduino to pin Echo of HC-SR04
 
-// Ultrasonic Sensor L1_B4
+// Ultrasonic Sensor 4
 #define trigPin4 6  // Attach pin 6 Arduino to pin Trig of HC-SR04
 #define echoPin4 7  // Attach pin 7 Arduino to pin Echo of HC-SR04
 
-// Ultrasonic Sensor for EXIT GATE
-#define trigPinG 8  // Attach pin 8 Arduino to pin Trig of HC-SR04
-#define echoPinG 9  // Attach pin 9 Arduino to pin Echo of HC-SR04
 
 /* 
  * Defining the number of registers we will be talking to: 
@@ -62,12 +51,36 @@ int angle = 0;
  * addressable binary outputs.
  */
 int numberOfRegisters = 2;
+int testCounter = 0;
 
 /*
  * Setup group of shift registers and name it `msr`.
  * All outputs are initialized to low/off.
  */
 MultiShiftRegister msr (numberOfRegisters , latchPin, clockPin, dataPin); 
+
+/*
+Function that test if the ultrasonic sensors are working or not
+:param distance: the distance returned by the ultrasonic sensor
+:param slotNumber: the slot we are testing
+:param expectedOutput: the expected output
+*/
+void testUltrasonic(int distance, String slotNumber, String expectedOutput) {
+  String testNum = "Starting test for: " + slotNumber;
+  Serial.println(testNum);
+  String output;
+
+  if(distance > 0 && distance <= 3){
+    output = "True";
+    //Serial.println(output);
+  }
+  else if(distance > 3) {
+    output = "False";
+    //Serial.println(output);
+  }
+  Serial.println((output == expectedOutput) ? "PASS\n": "FAIL\n");
+}
+
 
 void setup() {
   Serial.begin (9600);        // Begin Serial communication at a baudrate of 9600:
@@ -80,57 +93,24 @@ void setup() {
   * Trigger Pin : Set as Output
   * Echo Pin : Set as Input
   */
-  // Ultrasonic Sensor for Slot L1_B1
+  // Ultrasonic Sensor for Slot L2_A1
   pinMode(trigPin1, OUTPUT);  
   pinMode(echoPin1, INPUT);   
-  // Ultrasonic Sensor for Slot L1_B2
+  // Ultrasonic Sensor for Slot L2_A2
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
-  // Ultrasonic Sensor for Slot L1_B3
+  // Ultrasonic Sensor for Slot L2_A3
   pinMode(trigPin3, OUTPUT);
   pinMode(echoPin3, INPUT);
-  // Ultrasonic Sensor for Slot L1_B4
+  // Ultrasonic Sensor for Slot L2_A4
   pinMode(trigPin4, OUTPUT);
   pinMode(echoPin4, INPUT);  
-  // Ultrasonic Sensor for the Gate
-  pinMode(trigPinG, OUTPUT);
-  pinMode(echoPinG, INPUT);  
-
-  servo_exit.attach(10);  // Attach pin 10 Arduino to signal pin of servo    
-  servo_exit.write(0);    // Sets the initial servo position to 0 (Horizontal)
-  
 }
 
 void loop() {
 
-  /* 
-   * Ultrasonic Sensor For the Gate: 
-   */
-  long durationG, distanceG;
-  digitalWrite(trigPinG, HIGH);  // Added this line
-  delay(150);
-  digitalWrite(trigPinG, LOW);
-
-  durationG = pulseIn(echoPinG, HIGH);  // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
-  distanceG = (durationG/2) / 29.1;     // Convert the time into a distance
-
-  // Opening the Gate:
-  if(distanceG > 0 && distanceG <= 5) {
-    // This loop is used to slow down the servo
-    for(angle = 0; angle < 90; angle += 1){   // Command to move from 0 degrees to 90 degrees                               
-      servo_exit.write(angle);                // Command to rotate the servo to the specified angle
-      delay(15);                              // Delay for 15ms
-    }
-    servo_exit.detach();                      // This will stop the servo being in loop
-  }
-  // Closing the Gate:
-  else if(distanceG > 3) {    
-    servo_exit.attach(10);
-    servo_exit.write(0);
-  }
-    
-  /* 
-   * Ultrasonic Sensor For Parking Slot L1_B1
+/* 
+   * Ultrasonic Sensor For Parking Slot L2_A1
    */
   long duration, distance;
   // Trigger the sensor by setting the trigPin high for 150ms:
@@ -142,7 +122,7 @@ void loop() {
   distance = (duration/2) / 29.1;
 
   if(distance > 0 && distance <= 3){
-    Serial.println("L1_B1;True");
+    //Serial.println("L2_A1;True");
     msr.clear_shift(1);   // Green LED is off
     msr.clear_shift(2);   // Blue LED is off
     delay(50);            // Delay 50ms
@@ -150,7 +130,7 @@ void loop() {
 
   }
   else if(distance > 3) {
-    Serial.println("L1_B1;False");
+    //Serial.println("L2_A1;False");
     msr.clear_shift(0);   // Red LED is off
     msr.clear_shift(2);   // Blue LED is off
     delay(50);            // Delay 50ms
@@ -158,7 +138,7 @@ void loop() {
   }
   
   /* 
-   * Ultrasonic Sensor Parking Slot L1_B2
+   * Ultrasonic Sensor Parking Slot L2_A2
    */
   long duration2, distance2;
   // Trigger the sensor by setting the trigPin high for 150ms:
@@ -170,7 +150,7 @@ void loop() {
   distance2 = (duration2/2) / 29.1;
 
   if(distance2 > 0 && distance2 <= 3){
-    Serial.println("L1_B2;True");
+    //Serial.println("L2_A2;True");
     msr.clear_shift(4);   // Green LED is off
     msr.clear_shift(5);   // Blue LED is off
     delay(50);            // Delay 50ms
@@ -178,7 +158,7 @@ void loop() {
     
   }
   else if(distance2 > 3) {
-    Serial.println("L1_B2;False");
+    //Serial.println("L2_A2;False");
     msr.clear_shift(3);   // Red LED is off
     msr.clear_shift(5);   // Blue LED is off
     delay(50);            // Delay 50ms
@@ -186,7 +166,7 @@ void loop() {
   }
 
   /* 
-   * Ultrasonic Sensor For Parking Slot L1_B3
+   * Ultrasonic Sensor For Parking Slot L2_A3
    */
   long duration3, distance3;
   // Trigger the sensor by setting the trigPin high for 150ms:
@@ -198,7 +178,7 @@ void loop() {
   distance3 = (duration3/2) / 29.1;
 
   if(distance3 > 0 && distance3 <= 3){
-    Serial.println("L1_B3;True");
+    //Serial.println("L2_A3;True");
     msr.clear_shift(7);   // Green LED is off
     msr.clear_shift(8);   // Blue LED is off
     delay(50);            // Delay 50ms
@@ -206,7 +186,7 @@ void loop() {
     
   }
   else if(distance3 > 3) {
-    Serial.println("L1_B3;False");
+    //Serial.println("L2_A3;False");
     msr.clear_shift(6);   // Red LED is off
     msr.clear_shift(8);   // Blue LED is off
     delay(50);            // Delay 50ms
@@ -214,7 +194,7 @@ void loop() {
   }
 
   /* 
-   * Ultrasonic Sensor For Parking Slot L1_B4
+   * Ultrasonic Sensor For Parking Slot L2_A4
    */
   long duration4, distance4;
   // Trigger the sensor by setting the trigPin high for 150ms:
@@ -226,7 +206,7 @@ void loop() {
   distance4 = (duration4/2) / 29.1;
 
   if(distance4 > 0 && distance4 <= 3){
-    Serial.println("L1_B4;True");
+    //Serial.println("L2_A4;True");
     msr.clear_shift(10);  // Green LED is off
     msr.clear_shift(11);  // Blue LED is off
     delay(50);            // Delay 50ms
@@ -234,11 +214,33 @@ void loop() {
     
   }
   else if(distance4 > 3) {
-    Serial.println("L1_B4;False");
+    //Serial.println("L2_A4;False");
     msr.clear_shift(9);   // Red LED is off
     msr.clear_shift(11);  // Blue LED is off
     delay(50);            // Delay 50ms
     msr.set_shift(10);    // Green LED is on
   }
+
+ // Code to test if something is in front of the sensor
+  if (testCounter == 0) {
+    Serial.println("TEST 1: CAR IS PRESENT IN FRONT OF ALL SENSORS");
+    Serial.println("EXPECTED OUTPUT: ALL SENSORS ARE RED.\n");
+    testUltrasonic(distance, "L2_B1", "True");
+    testUltrasonic(distance2, "L2_B2", "True");
+    testUltrasonic(distance3, "L2_B3", "True");
+    testUltrasonic(distance4, "L2_B4", "True");
+  }
+  // Code to test if nothing is in front of the sensor
+  // counter == 5 to allow for time to empty the parking slots
+  else if (testCounter == 5) {
+    Serial.println("TEST 2: NO CAR PRESENT IN FRONT OF ALL SENSORS");
+    Serial.println("EXPECTED OUTPUT: ALL SENSORS ARE GREEN.\n");
+    testUltrasonic(distance, "L2_B1", "False");
+    testUltrasonic(distance2, "L2_B2", "False");
+    testUltrasonic(distance3, "L2_B3", "False");
+    testUltrasonic(distance4, "L2_B4", "False");
+  }
+
+  testCounter++;
 
 }
